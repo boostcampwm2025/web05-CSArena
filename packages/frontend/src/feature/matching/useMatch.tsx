c99a48178e269857ce3228ed7afa19d57c58af13
황재호 <72853328+woghrk12@users.noreply.github.com>
2026-01-12 15:02:43 +0900

    refactor: 소켓 연결 시점을 Home 화면에서 QUICK START 버튼 클릭했을 때에서 Match Provider가 마운트 되는 시점으로 수정

--- packages/frontend/src/feature/matching/useMatch.tsx
+++ packages/frontend/src/feature/matching/useMatch.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext } from 'react';
 import { useCallback, useEffect, useRef, useState } from 'react';
 
-import { MatchFound } from '@/lib/socket/event';
+import { MatchEnqueueRes, MatchFound } from '@/lib/socket/event';
 import { getSocket } from '@/lib/socket';
 
 type MatchState = 'matching' | 'inGame';
@@ -20,6 +20,17 @@ export function MatchProvider({ children }: { children: React.ReactNode }) {
 
   const socketRef = useRef(getSocket());
 
+  const handleConnect = useCallback(() => {
+    const socket = socketRef.current;
+
+    socket.emit('match:enqueue', undefined, (ack: MatchEnqueueRes) => {
+      if (!ack.ok) {
+        // TODO: 소켓 연결 실패 시 메인 화면으로 돌아가거나 자동 재연결 등 에러 헨들링 로직 추가
+        return;
+      }
+    });
+  }, []);
+
   const handleMatchFound = useCallback((payload: MatchFound) => {
     setOpponentInfo(payload.opponent);
     setMatchState('inGame');
@@ -28,12 +39,22 @@ export function MatchProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const socket = socketRef.current;
 
+    socket.on('connect', handleConnect);
     socket.on('match:found', handleMatchFound);
 
+    if (!socket.connected) {
+      socket.connect();
+    } else {
+      handleConnect();
+    }
+
     return () => {
+      socket.off('connect', handleConnect);
       socket.off('match:found', handleMatchFound);
+
+      socket.disconnect();
     };
-  }, [handleMatchFound]);
+  }, [handleConnect, handleMatchFound]);
 
   return <MatchCtx.Provider value={{ matchState, opponentInfo }}>{children}</MatchCtx.Provider>;
 }
--- packages/frontend/src/pages/home/Home.tsx
+++ packages/frontend/src/pages/home/Home.tsx
@@ -1,7 +1,4 @@
-import { useState, useRef } from 'react';
-
-import { MatchEnqueueRes } from '@/lib/socket/event';
-import { getSocket } from '@/lib/socket';
+import { useState } from 'react';
 
 import { useScene } from '@/feature/useScene.tsx';
 
@@ -9,7 +6,6 @@ export default function Home() {
   const { setScene } = useScene();
 
   const [isDisabled, setIsDisabled] = useState<boolean>(false);
-  const socketRef = useRef(getSocket());
 
   const onClickQuickStartBtn = () => {
     if (isDisabled) {
@@ -17,24 +13,7 @@ export default function Home() {
     }
 
     setIsDisabled(true);
-
-    const socket = socketRef.current;
-
-    socket.once('connect', () => {
-      // 소켓 연결 완료 후 큐 진입 요청 이벤트 호출
-      socket.emit('match:enqueue', undefined, (ack: MatchEnqueueRes) => {
-        if (!ack.ok) {
-          throw new Error();
-        }
-
-        // 큐 진입 응답이 오면 매치 씬으로 전환
-        setIsDisabled(false);
-        setScene('match');
-      });
-    });
-
-    // 소켓 연결 시도
-    socket.connect();
+    setScene('match');
   };
 
   return (
