dd0686b6f93c0060bc9399ce579abd810fe9fcd5
PSW99 <steamdayz@naver.com>
2026-01-15 16:16:19 +0900

    feat: single-play 모듈 추가
    - SinglePlayModule을 생성하여 단일 플레이 관련 기능을 모듈화.
    - AppModule에 SinglePlayModule 등록으로 모듈 연결.

---
+++ packages/backend/src/single-play/single-play.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SinglePlayController } from './single-play.controller';
+import { Category, Question } from '../quiz/entity';
+import { QuizService } from '../quiz/quiz.service';
+import { ClovaClientService } from '../quiz/clova/clova-client.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Category, Question])],
+  controllers: [SinglePlayController],
+  providers: [QuizService, ClovaClientService],
+})
+export class SinglePlayModule {}
