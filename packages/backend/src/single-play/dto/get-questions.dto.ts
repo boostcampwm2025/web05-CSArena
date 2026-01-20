b31e1f4e62cef4016ac7e587b927bb85e7611e8a
PSW99 <steamdayz@naver.com>
2026-01-20 14:18:16 +0900

    feat: GetQuestionsDto categoryId 필드 타입 변경 및 유효성 검사 추가
    - categoryId 타입을 문자열에서 배열로 변경.
    - Transform 데코레이터를 사용하여 문자열 데이터를 배열로 변환 처리 추가.
    - 각 categoryId가 숫자인지 검증하는 로직과 배열 유효성 검사 추가.

--- packages/backend/src/single-play/dto/get-questions.dto.ts
+++ packages/backend/src/single-play/dto/get-questions.dto.ts
@@ -1,7 +1,19 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsArray, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class GetQuestionsDto {
   @IsNotEmpty({ message: 'categoryId는 필수입니다.' })
-  @IsString({ message: 'categoryId는 문자열이어야 합니다.' })
-  categoryId: string;
+  @Transform(({ value }: { value: unknown }) => {
+    if (typeof value === 'string') {
+      return value
+        .split(',')
+        .map((id: string) => parseInt(id.trim(), 10))
+        .filter((id: number) => !isNaN(id));
+    }
+
+    return value;
+  })
+  @IsArray({ message: 'categoryId는 배열이어야 합니다.' })
+  @IsNumber({}, { each: true, message: '각 categoryId는 숫자여야 합니다.' })
+  categoryId: number[];
 }
